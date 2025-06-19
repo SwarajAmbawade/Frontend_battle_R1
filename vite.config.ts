c6933af57883b3855517e38bb23e93dc6915f5fa
SwarajAmbawade <swaraj78600@gamil.com>
2025-06-19 17:11:50 +0530

    Fix: Set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Frontend_battle_R1/", // ✅ GitHub repo name
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
+    port: 8888,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
